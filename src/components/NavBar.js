816933832717954be32bc8ae46fca11b03cf8be1
ThallytaMedeiros <96738776+ThallytaM@users.noreply.github.com>
2022-11-11 15:31:47 -0300

    atualização navBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,41 @@
 import React from 'react';
+import logo from '../img/logo192.png';
 
 import NavBarItem from './NavBarItem';
 
+
 function NavBar(){
+
     return(
         <div className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-        <a className="navbar-brand" href="/">MI</a>
+        <a className="navbar-brand" href="/">
+        <img src = {logo} width="60" height="60" class="d-inline-block align-top" alt=""/></a>
         
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
         </button>
-        
+  
         <div className="collapse navbar-collapse" id="navbarColor01">
+        <div className="mr-auto"></div>
         <ul className="navbar-nav me-auto">
             <NavBarItem href = "/" label = 'Home'/>
             <NavBarItem href = "/login" label = 'Login' />
             <NavBarItem href = "/viewClient" label = 'Clientes'/>
             <NavBarItem href = "/createClient" label = 'Novo Cliente'/>
-            <NavBarItem href = "/updateClient" label = 'Atualizar Cliente'/>
-            <NavBarItem href = "/deleteClient" label = 'Excluir Cliente'/>
             <NavBarItem href = "/viewProperty" label = 'Propriedades'/>
             <NavBarItem href = "/createProperty" label = 'Nova Propriedade'/>
-            <NavBarItem href = "/updateProperty" label = 'Atualizar Propriedade'/>
-            <NavBarItem href = "/deleteProperty" label = 'Excluir Propriedade'/>
             <NavBarItem href = "/viewContract" label = 'Contratos'/>
             <NavBarItem href = "/createContract" label = 'Novo Contrato'/>
-            <NavBarItem href = "/updateContract" label = 'Atualizar Contrato'/>
-            <NavBarItem href = "/deleteContract" label = 'Excluir Contrato'/>
-        
-
         </ul> 
+           
+               
+                </div>
+              </div>
+            </div> 
+
         
-    </div>
-  </div>
-</div>
+  
     )
 }
 
